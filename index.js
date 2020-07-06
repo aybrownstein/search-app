7ba7518476141dae098c72b8d0ec9b96a2b94d46
Aharon Brownstein <aybrownstein@gmail.com>
2020-07-06 16:43:32 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+const searchUrl = 'https://api.github.com/users/'
+
+function formatQueryParams(params) {
+    const queryItems = object.keys(params)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems;
+}
+
+function displayResults(responseJson) {
+    console.log(responseJson);
+    $('#results-list').empty();
+    $('#results-list').append(
+        `<li><h3><a href="${responseJson.url}">${responseJson.title}</a></h3></li>`
+    );
+    $('#results').removeClass('hidden')
+};
+
+function getRepo(query) {
+    const params = {
+        q: query,
+    };
+    const queryString = formatQueryParams(params)
+    const url = searchUrl + queryString + '/repos';
+    console.log(url);
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(err => { $('#js-error-message').text(`something went wrong:${err.message}`); });
+}
+
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        const searchTerm = $('#js-results').val();
+        getRepo(searchTerm);
+    });
+}
+$(watchForm);
